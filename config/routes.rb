6be9d402f57a29f714feb3cc28dca923bc001341
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-09-05 14:18:49 -0600

    Add users - users can sign in and out with Spotify

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/auth/spotify/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :artists do
     resources :songs, only: [:new, :create]
   end
